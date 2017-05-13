b40a7ec8e036bf6536bea5ac2a8aa6dc62f0a895
Ivan Zhou <ivanzhou@gmail.com>
2017-05-12 22:37:01 -0700

    first working version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,6 @@
+require 'pp'
+require 'twitter'
+
 class SessionsController < ApplicationController
   def create
     credentials = request.env['omniauth.auth']['credentials']
@@ -9,9 +12,54 @@ class SessionsController < ApplicationController
   def show
     if session['access_token'] && session['access_token_secret']
       @user = client.user(include_entities: true)
+      friends = get_friends
+      friends.select! do |f|
+        f[:description].downcase.include?("snap")
+      end
+
+      @friends = friends.map do |user|
+        user[:description].gsub!(":", " ")
+        user[:description].gsub!("@", " ")
+        pieces = user[:description].split(" ")
+        i = 0
+        found = false
+        snapchat = ""
+        while i < pieces.length
+          if pieces[i].downcase.include?("snap")
+            found = true
+            i += 1
+            next
+          end
+
+          if found && (pieces[i] =~ /[a-zA-Z0-9]+[a-zA-Z0-9\.\-\_]+/) != nil
+            if pieces[i].downcase.include?("insta") ||
+              pieces[i].downcase == "chat" ||
+              pieces[i].length < 5
+              i += 1
+              next 
+            end
+            snapchat = pieces[i]
+            break
+          end
+          i += 1
+        end
+        {:name => user[:name], :snapchat => snapchat}
+      end
     else
       redirect_to failure_path
     end
+  end
+
+  def get_friends
+    cursor = -1
+    friends = []
+    begin
+      f = client.friends(:cursor => cursor, :count => 200, include_entities: true)
+      friends += f.attrs[:users]
+      cursor = f.attrs[:next_cursor]
+      pp cursor
+    end while cursor > 0
+    return friends
   end
 
   def error
@@ -23,4 +71,5 @@ class SessionsController < ApplicationController
     reset_session
     redirect_to root_path, notice: 'Signed out'
   end
+
 end
